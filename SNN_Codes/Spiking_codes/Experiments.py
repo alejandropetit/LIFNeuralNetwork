0cb75a3e5b2cac9ebe6f01f043f54cd1e74358d6
Alejandro Petit <alejandropetit11@gmail.com>
2026-08-17 12:11:10 -0500

    changes for test

--- SNN_Codes/Spiking_codes/Experiments.py
+++ SNN_Codes/Spiking_codes/Experiments.py
@@ -11,6 +11,6 @@ import SNN_train_controller as ctrl
 fail = False
 #m = ctrl.SNN_complete_train_test()
 #fail = m.train_SNN()
-m = ctrl.SNN_complete_train_test()
+#m = ctrl.SNN_complete_train_test()
 if not fail:
     m.test_SNN()
