5258fea7ce9802ebc6ea0ff41fbbe0d822f02939
Alejandro Petit <alejandropetit11@gmail.com>
2026-08-17 15:01:10 -0500

    fix error

--- SNN_Codes/Spiking_codes/Experiments.py
+++ SNN_Codes/Spiking_codes/Experiments.py
@@ -9,7 +9,7 @@ Created on Mon Feb  7 15:54:21 2022
 import SNN_train_controller as ctrl
 
 fail = False
-#m = ctrl.SNN_complete_train_test()
+m = ctrl.SNN_complete_train_test()
 #fail = m.train_SNN()
 #m = ctrl.SNN_complete_train_test()
 if not fail:
