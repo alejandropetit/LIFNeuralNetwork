206275fdd08ae2891b0c3de86c6bf7c31f1f21ac
Alejandro Petit <alejandropetit11@gmail.com>
2026-08-17 00:22:17 -0500

    change of a directory

--- Initial_scripts/start_experiment_D.sh
+++ Initial_scripts/start_experiment_D.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 cd /home/nelson
 source /home/nelson/Puerto_serie.sh &
-source /home/nelson/Ubuntu_master/Initial_scripts/sail_init.sh &
+source /home/nelson/sail_init.sh &
 source Anaconda.sh
 python Documentos/Ubuntu_master/SNN_Codes/Spiking_codes/Experiments.py
-exit
-exit
 exit
 
