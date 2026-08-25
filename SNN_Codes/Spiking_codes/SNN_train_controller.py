14e322530784777e585d35e5cc93d4b40313f63d
Alejandro Petit <alejandropetit11@gmail.com>
2026-08-25 00:55:06 -0500

    correct case-sensitive naming

--- SNN_Codes/Spiking_codes/SNN_train_controller.py
+++ SNN_Codes/Spiking_codes/SNN_train_controller.py
@@ -11,7 +11,7 @@ class SNN_complete_train_test:
     port_name='interface_1'
     direction = '/home/nelson/Documentos/Ubuntu_master/SNN_Codes/Spiking_codes'
     timeout=15
-    p = sp.serial_port(direction, port_name, timeout)
+    p = sp.Serial_Port(direction, port_name, timeout)
     config_file = 'config.txt'
     
     # Error ranges for each variable[0,1] and pitch range
